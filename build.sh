b38732009e7002722e93a36f3ce60daedb4bcd68
David Claeys <davidclaeys@outlook.com>
2026-02-19 14:38:31 +0100

    update build script to 2.0.0 release

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BUILD_DEPENDENCIES="git nano cmake ninja-build devscripts build-essential lintian python3 python3-dev python3-pip python3-jsonschema  python3-jinja2 qt6-base-dev qt6-serialport-dev qt6-websockets-dev libudev-dev libxkbcommon-dev libvulkan-dev libgl1-mesa-dev libusb-1.0-0-dev libasound2-dev libturbojpeg0-dev libjpeg-dev libssl-dev pkg-config libftdi1-dev libcec-dev libp8-platform-dev libudev-dev libdrm-dev libxcb-randr0-dev libxcb-shm0-dev libxcb-image0-dev libx11-dev libxrandr-dev libxrender-dev libxcb-util0-dev libxcb-render0-dev"
-HYPERION_RELEASE_TAG="2.1.1"
+HYPERION_RELEASE_TAG="2.2.0"
 HYPERION_GIT_REPO_URL="https://github.com/hyperion-project/hyperion.ng.git"
 BUILD_TYPE="Release"
 ARCHITECTURE="amd64"
